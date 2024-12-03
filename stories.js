ec22b4e07f43f1828d4e9ff77aa8c7ab982504a4
Robso <robsonpcpereira15@gmail.com>
2024-12-03 13:15:38 -0300

    Add files via upload

---
+++ stories.js
@@ -0,0 +1,39 @@
+var crl1 = document.getElementById('myCrl1').getContext('2d');
+
+crl1.beginPath();
+crl1.arc(60, 100, 40, 0, 2 * Math.PI);
+crl1.fillStyle = "#4169E1";
+crl1.fill();
+crl1.lineWidth = 5;
+crl1.strokeStyle = "#dbdbdb";
+crl1.stroke();
+
+var crl2 = document.getElementById('myCrl2').getContext('2d');
+
+crl2.beginPath();
+crl2.arc(60, 100, 40, 0, 2 * Math.PI);
+crl2.fillStyle = "#87CEFA";
+crl2.fill();
+crl2.lineWidth = 5;
+crl2.strokeStyle = "#dbdbdb";
+crl2.stroke();
+
+var crl3 = document.getElementById('myCrl3').getContext('2d');
+
+crl3.beginPath();
+crl3.arc(60, 100, 40, 0, 2 * Math.PI);
+crl3.fillStyle = "#4682B4";
+crl3.fill();
+crl3.lineWidth = 5;
+crl3.strokeStyle = "#dbdbdb";
+crl3.stroke();
+
+var crl4 = document.getElementById('myCrl4').getContext('2d');
+
+crl4.beginPath();
+crl4.arc(60, 100, 40, 0, 2 * Math.PI);
+crl4.fillStyle = "#B0C4DE";
+crl4.fill();
+crl4.lineWidth = 5;
+crl4.strokeStyle = "#dbdbdb";
+crl4.stroke();
